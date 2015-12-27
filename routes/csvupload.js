46f9537626f484820f71d0097de897db6023f640
Dorota Kawalec <dorota.m.kawalec@gmail.com>
2015-12-27 20:47:05 +0100

    + CSV parser server-side

--- routes/csvupload.js
+++ routes/csvupload.js
@@ -9,7 +9,7 @@ router.get('/', function (req, res, next) {
   res.render('csv', {});
 });
 
-/* POST csv users listing. */
+/* POST csv users listing for conversion. */
 router.post('/convert', function (req, res, next) {
   if (req.files && req.files.csvFile) { 
     var filePath = req.files.csvFile.path;
@@ -87,5 +87,50 @@ function parseChunk(chunk) {
   }
   return result;
 };
+
+/* POST csv users listing to get JSON usage data. */
+router.post('/', function (req, res, next) {
+  if (req.files && req.files.csvFile) { 
+    var filePath = req.files.csvFile.path;
+    if (req.files.csvFile.extension === 'csv') {
+      var fileName = req.files.csvFile.name;
+      
+      fs.readFile(filePath, {encoding: 'utf-8'}, function (err, data) {
+        if (err) res.status(400).render('error', { message: 'Error processing file', error: err });
+        else {
+          csv.parse(data, {comment: '//', delimiter: ';', rowDelimiter: '\n'}, function (err, output) {
+            if (err) res.status(400).render('error', { message: 'Error parsing CSV file. Check file syntax before trying again', error: err });
+            else {
+              res.status(200).json(extractData(output));
+              fs.unlinkSync(filePath);
+            }
+          });
+        }
+      });
+    } else {
+      res.status(400).render('error', { message: 'Wrong file type! Only CSV files are accepted.', error: { status: 400 } });
+      fs.unlinkSync(filePath);
+    }
+  } else res.status(400).render('error', { message: 'No file was sent.', error: { status: 400 } });
+});
+
+// array of arrays
+function extractData(array) {
+  return array.filter(function(e) {
+    return e instanceof Array && e.length >= 8;
+  }).map(function(e, i) {
+    var numberOfInputs = parseInt(e[2]),
+      numberOfOutputs = parseInt(e[3+numberOfInputs]),
+      offsetTotal = numberOfInputs+numberOfOutputs;
+      if (i<10) console.log(e, i, ' --- ', numberOfInputs, numberOfOutputs, offsetTotal)
+    return {
+      nodeId: e[0],
+      startTime: e[1],
+      sources: e.slice(3, 3+numberOfInputs),
+      targets: e.slice(4+numberOfInputs, 4+offsetTotal),
+      duration: e[4+offsetTotal] + e[5+offsetTotal]
+    };
+  });
+}
 
 module.exports = router;
