07665ef27989f7f25415beffb2e9ec012a43bc1b
Dorota Kawalec <dorota.m.kawalec@gmail.com>
2015-12-27 16:41:14 +0100

    + proper connections array

--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -2,23 +2,32 @@ app.controller('NoSViewController', ['$scope', '$document', 'FilesService', func
   $scope.xmlData = {};
   $scope.csvData = {};
 
-  $scope.nos = undefined;
+  $scope.nos = {
+      graph: {}
+  };
 
   $scope.stats = {};
 
   function calculateStats() {
-    var nodes = $scope.nos,
-      inputs = Array.concat.apply([], nodes.map(function(e) {
-        return e.functionalDescription.inputs;
+    var nodes = $scope.nos.graph.nodes,
+      inputs = Array.concat.apply([], nodes.map(function(e, i) {
+        return e.functionalDescription.inputs.map(function(f) {
+          return { data: f, nodeId: e.nodeId, number: i };
+        });
       })),
-      outputs = Array.concat.apply([], nodes.map(function(e) {
-        return e.functionalDescription.outputs;
+      outputs = Array.concat.apply([], nodes.map(function(e, i) {
+        return e.functionalDescription.outputs.map(function(f) {
+          return { data: f, nodeId: e.nodeId, number: i };
+        });
       })),
-      connections = 0;
-    inputs.forEach(function(e) {
-      connections += outputs.filter(function(f) {
-        return f.dataType === e.dataType;
-      }).length;
+      connections = [];
+    outputs.forEach(function(e) {
+      var i = e.number;
+      connections = connections.concat(inputs.map(function(f) {
+        return { source: i, target: f.number, dataType: f.data.dataType }
+      }).filter(function(f) {
+        return f.dataType === e.data.dataType;
+      }));      
     });
 
     $scope.stats.numberOfServices = nodes.length;
@@ -26,15 +35,17 @@ app.controller('NoSViewController', ['$scope', '$document', 'FilesService', func
     $scope.stats.inputsAverage = inputs.length/nodes.length;
     $scope.stats.outputsTotal = outputs.length;
     $scope.stats.outputsAverage = outputs.length/nodes.length;
-    $scope.stats.connectionsTotal = connections;
-    $scope.stats.connectionsAverage = connections/nodes.length;
+    $scope.stats.connectionsTotal = connections.length;
+    $scope.stats.connectionsAverage = connections.length/nodes.length;
+    
+    $scope.nos.graph.connections = connections;
   }
 
   $scope.uploadXML = function() {
     $scope.xmlData.files = document.querySelectorAll('#xmlFile')[0].files;
     FilesService.uploadXML($scope.xmlData)
     .then(function(response) {
-      $scope.nos = response.data.graph.nodes;
+      $scope.nos = response.data;
       calculateStats();
     }).catch(function(error) {
       console.log(error);
