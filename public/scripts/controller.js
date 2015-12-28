d2b3d599e4302d67bebe909468164592e16a2608
Dorota Kawalec <dorota.m.kawalec@gmail.com>
2015-12-28 20:31:27 +0100

    + DNoS stats calculation and display

--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -5,11 +5,13 @@ app.controller('NoSViewController', ['$scope', '$document', 'FilesService', func
   $scope.nos = {
       graph: {}
   };
-  $scope.dnos = {};
+  $scope.dnos = {
+    connections: []
+  };
 
   $scope.stats = {};
 
-  function calculateStats() {
+  function calculateNoSStats() {
     var nodes = $scope.nos.graph.nodes,
       inputs = Array.concat.apply([], nodes.map(function(e, i) {
         return e.functionalDescription.inputs.map(function(f) {
@@ -31,6 +33,7 @@ app.controller('NoSViewController', ['$scope', '$document', 'FilesService', func
       }));      
     });
 
+    $scope.stats = {};
     $scope.stats.numberOfServices = nodes.length;
     $scope.stats.inputsTotal = inputs.length;
     $scope.stats.inputsAverage = inputs.length/nodes.length;
@@ -40,6 +43,32 @@ app.controller('NoSViewController', ['$scope', '$document', 'FilesService', func
     $scope.stats.connectionsAverage = connections.length/nodes.length;
     
     $scope.nos.graph.connections = connections;
+    $scope.nos.nodeIds = nodes.map(function(e) {
+      return e.nodeId;
+    });
+  }
+
+  function calculateDNoSStats() {
+    var times, first, last;
+    $scope.dnos.connections = $scope.dnos.all.filter(function(e) {
+      return $scope.nos.nodeIds.some(function(f) {
+        return e.sources.indexOf(f) !== -1 || e.targets.indexOf(f) !== -1;
+      });
+    });
+    times = $scope.dnos.connections.map(function(e) {
+      return parseInt(e.startTime, 10);
+    }),
+    first = times.reduce(function (e, f) {
+      return e < f ? e : f;
+    }),
+    last = times.reduce(function (e, f) {
+      return e > f ? e : f;
+    });
+    
+    $scope.stats.totalCalls = $scope.dnos.all.length;
+    $scope.stats.repositoryCalls = $scope.dnos.connections.length;
+    $scope.stats.usableData = Math.floor($scope.stats.repositoryCalls/$scope.stats.totalCalls * 100) + '%';
+    $scope.stats.totalTime = Math.floor((last-first) / 1000) + ' seconds';
   }
 
   $scope.uploadXML = function() {
@@ -47,7 +76,7 @@ app.controller('NoSViewController', ['$scope', '$document', 'FilesService', func
     FilesService.uploadXML($scope.xmlData)
     .then(function(response) {
       $scope.nos = response.data;
-      calculateStats();
+      calculateNoSStats();
     }).catch(function(error) {
       console.log(error);
     });
@@ -57,8 +86,8 @@ app.controller('NoSViewController', ['$scope', '$document', 'FilesService', func
     $scope.csvData.files = document.querySelectorAll('#csvFile')[0].files;
     FilesService.uploadCSV($scope.csvData)
     .then(function(response) {
-      console.log(response.data);
-      $scope.dnos = response.data;
+      $scope.dnos.all = response.data;
+      calculateDNoSStats();
     }).catch(function(error) {
       console.log(error);
     });
